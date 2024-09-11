e637c253c1393f15cd6c95891ecb023e6779e5e1
aswathik662 <aswathikudayan@gmail.com>
2024-09-11 07:32:24 +0530

    Add files via upload

---
+++ pp.py
@@ -0,0 +1,26 @@
+# list=[i for i in range(1,50)if i%3==0 and i%5==0]
+# print(list)
+
+
+# n=input("enter the strings:")
+# s={'u', 'a', 'e', 'i', 'o'}
+# set={i for i in n if n in s }
+# print(set)
+
+# set={i**2 for i in range(1,15) if i**2%2!=0 }
+# print(set)
+
+# dic={x:len(x) for x in ['apple', 'banana', 'cherry', 'date']}
+# print(dic)
+
+ 
+
+
+# n={1,2,3,4,5,6,7,8,9,10}
+# dict={n:('true' if n%2==0 else 'false')for n in n}
+# print(dict)
+
+# n=input("enter the strings:")
+# s={'u', 'a', 'e', 'i', 'o'}
+# set={i for i in n.lower() if i in s }
+# print(set)
